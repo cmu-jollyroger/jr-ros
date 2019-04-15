68c615adf8325b3f7794ffc64a451e5c67ea004a
FionaLi <siliangl@andrew.cmu.edu>
2019-04-15 18:52:22 -0400

    new msg added, communication reimplemented based on new chassis_info_t

--- src/jr_communication/src/jr_comm_info_node.cpp
+++ src/jr_communication/src/jr_comm_info_node.cpp
@@ -7,8 +7,9 @@
 #include <Info_package/TOF_info.msg>
 #include <Info_package/Motor_info.msg>
 
-ros::Publisher motor_info_pub;
+ros::Publisher chassis_info_pub;
 ros::Publisher tof_info_pub;
+ros::Publisher motor_pid_info_pub;
 #define MSG_QUEUE_SIZE (1)
 
 typedef enum
@@ -17,6 +18,36 @@ INFO_TOF = 0,
 INFO_MOTOR  = 1,
 } info_type;
 
+
+typedef __packed struct
+{
+uint8_t ctrl_mode;      /**< chassis control mode */
+float   gyro_palstance; /**< chassis palstance(degree/s) from gyroscope */
+float   gyro_angle;     /**< chassis angle(degree) relative to ground from gyroscope */
+float   ecd_palstance;  /**< chassis palstance(degree/s) from chassis motor encoder calculated */
+float   ecd_calc_angle; /**< chassis angle(degree) relative to ground from chassis motor encoder calculated */
+int16_t x_spd;          /**< chassis x-axis move speed(mm/s) from chassis motor encoder calculated */
+int16_t y_spd;          /**< chassis y-axis move speed(mm/s) from chassis motor encoder calculated */
+int32_t x_position;     /**< chassis x-axis position(mm) relative to the starting point */
+int32_t y_position;     /**< chassis y-axis position(mm) relative to the starting point */
+int16_t tof_0;
+int16_t tof_1;
+int16_t tof_2;
+int16_t tof_3;
+int16_t tof_4;
+int16_t tof_5;
+float spd_p;
+float spd_i;
+float spd_d;
+float pos_p;
+float pos_i;
+float pos_d;
+uint8_t sw_l;           /**< left limit switch */
+uint8_t sw_r;           /**< right limit switch */
+} chassis_info_t;
+
+
+
 void data_handle(uint8_t* protocol_packet) {
   frame_header_t *p_header = (frame_header_t *)p_frame;
   memcpy(p_header, p_frame, HEADER_LEN);
@@ -24,28 +55,34 @@ void data_handle(uint8_t* protocol_packet) {
   uint16_t data_length = p_header->data_length;
   uint16_t cmd_id      = *(uint16_t *)(p_frame + HEADER_LEN);
   uint8_t *data_addr   = p_frame + HEADER_LEN + CMD_LEN;
-  ros::Publisher pub;
-  if (p_header->sof == INFO_TOF) {
 
-  } else if (p_header->sof == INFO_MOTOR) {
-    Info_package::Motor_info msg;
-    msg.spd0 = data_addr[0];
-    msg.spd1 = data_addr[1];
-    msg.spd2 = data_addr[2];
-    msg.spd3 = data_addr[3];
-    motor_info_pub.publish(msg);
-    return;
+  chassis_info_t *chassis_info = (chassis_info_t)data_addr;
 
-  } else {
-    Info_package::TOF_info msg;
-    msg.dist0 = data_addr[0];
-    msg.dist1 = data_addr[1];
-    msg.dist2 = data_addr[2];
-    msg.dist3 = data_addr[3];
-    msg.dist4 = data_addr[4];
-    msg.dist5 = data_addr[5];
-    tof_info_pub.publish(msg);
-    return;
+  if (cmd_id == CHASSIS_DATA_ID) {
+    Info_package::TOF_info tof_msg;
+    tof_msg.dist0 = chassis_info->tof_0;
+    tof_msg.dist1 = chassis_info->tof_1;
+    tof_msg.dist2 = chassis_info->tof_2;
+    tof_msg.dist3 = chassis_info->tof_3;
+    tof_msg.dist4 = chassis_info->tof_4;
+    tof_msg.dist5 = chassis_info->tof_5;
+    tof_info_pub.publish(tof_msg);
+
+    Info_package::Chassis_info chassis_msg;
+    chassis_msg.x_spd = chassis_info->x_spd;
+    chassis_msg.y_spd = chassis_info->y_spd;
+    chassis_msg.x_position = chassis_info->x_position;
+    chassis_msg.y_position = chassis_info->y_position;
+    chassis_info_pub.publish(chassis_msg);
+
+    Info_package::Motor_PID_info motor_pid_msg;
+    motor_pid_msg.spd_p = chassis_info->spd_p;
+    motor_pid_msg.spd_i = chassis_info->spd_i;
+    motor_pid_msg.spd_d = chassis_info->spd_d;
+    motor_pid_msg.pos_p = chassis_info->pos_p;
+    motor_pid_msg.pos_i = chassis_info->pos_i;
+    motor_pid_msg.pos_d = chassis_info->pos_d;
+    tof_info_pub.publish(motor_pid_msg);
   }
 }
 
@@ -69,7 +106,7 @@ void *jrcom_recv_thread(void *argu) {
       switch (unpack_step) {
         case STEP_HEADER_SOF:
         {
-          if((byte == INFO_TOF)|| (byte == INFO_MOTOR))
+          if(byte == CHASSIS_INFO)
           {
             unpack_step = STEP_LENGTH_LOW;
             protocol_packet[index++] = byte;
@@ -177,10 +214,12 @@ int main(int argc, char **argv) {
 
   ros::NodeHandle node;
 
-  motor_info_pub = node.advertise<Info_package::Motor_info>(
-          "jr_motor", MSG_QUEUE_SIZE);
+  chassis_info_pub = node.advertise<Info_package::Chassis_info>(
+          "jr_chassis", MSG_QUEUE_SIZE);
   tof_info_pub = node.advertise<Info_package::TOF_info>(
           "jr_tof", MSG_QUEUE_SIZE);
+  motor_pid_info_pub = node.advertise<Info_package::Motor_PID_info>(
+          "jr_motor_pid", MSG_QUEUE_SIZE);
 
 
   // Running at 10Hz
