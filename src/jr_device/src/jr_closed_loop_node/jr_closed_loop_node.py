6b7ff79d8bc0dba87d5569861f6c82475bf2c09d
harveybia <harvey.shi@yahoo.com>
2019-05-07 03:58:15 -0400

    added webcam script, srv and roslaunch file

---
+++ src/jr_device/src/jr_closed_loop_node/jr_closed_loop_node.py
@@ -0,0 +1,13 @@
+import cv2
+
+if __name__ == "__main__":
+    print "hello world"
+
+    cap = cv2.VideoCapture('http://192.168.8.102:9000/?action=stream')
+
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) == 27:
+            exit(0)
